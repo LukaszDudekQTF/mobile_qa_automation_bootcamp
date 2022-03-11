2135233b2012ef7e3c60550c23dc0612afb79b44
Lukasz Dudek <ldudek@qtf.io>
2022-03-11 18:52:07 +0100

    Lesson3: setup/teardown class

--- test01.py
+++ test01.py
@@ -8,3 +8,15 @@ class Test01Android:
     def test_01(self):
         log.info("test_01")
         assert True
+
+    @classmethod
+    def setup_class(cls):
+        # set up some state
+        log.info("setup_class")
+        pass
+
+    @classmethod
+    def teardown_class(cls):
+        # teardown state
+        log.info("teardown_class")
+        pass
